a7c0181dc982be512efd5db48c233acfe63e918c
Cameron Rodgers <crr3@st-andrews.ac.uk>
2020-07-15 14:54:13 +0100

    Small bug fix: flags were disappearing when the user clicks on the 'view solver actions' button at the end of a game. This is now fixed. This commit also includes an inclusion of the new test folder. This will use mocha and chai.js to create small unit tests for my code, and provide a user display for testing feedback on the program as it is. Further work on this to follow.

--- content/client.js
+++ content/client.js
@@ -315,9 +315,6 @@ const showReplay = () => {
     // before they made a mistake
     let allNodes = document.querySelectorAll(".mineSquare")
     allNodes.forEach(node => {
-        if (!node.revealed) {
-            node.style.backgroundColor = ""
-        }
         if (node.childNodes.length > 0 && node.textContent == "") {
             // if there's a cross in the tile, remove it and put a flag back in it
             if (node.childNodes[0].getAttribute("class") == "cross") {
@@ -330,12 +327,21 @@ const showReplay = () => {
                 flag.style = "height: 100%; width: auto;"
                 node.append(flag)
             }
-            else if (node.childNodes.length == 2) {
+            else if (node.childNodes.length == 1) {
                 node.childNodes[0].style.display = "none"
+
+                if (node.style.backgroundColor == "orange") {
+                    // Display a flag in the square
+                    let flag = document.createElement("img")
+                    flag.src = "./img/flag.png"
+                    flag.setAttribute("class", "flag")
+                    flag.style = "height: 100%; width: auto;"
+                    node.append(flag)
+                }
             }
-            else if (node.childNodes.length == 1 && node.childNodes[0].getAttribute("class") == "mine") {
-                node.childNodes[0].style.display = "none"
-            }
+        }
+        if (!node.revealed) {
+            node.style.backgroundColor = ""
         }
     })
 
---
+++ test/clientTest.js
@@ -0,0 +1,10 @@
+// To run all client-side tests, simply open the console
+// in the web browser and run "runTest()" and full results will
+// be returned to the user
+const runTest = () => {
+    // the result array which will be logged to the console using console.table()
+    let result = []
+
+    let x = chai.assert.typeOf(currentGame, "string", "currentGame should be an object");
+    console.log(x)
+}
